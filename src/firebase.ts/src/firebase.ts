2faa37fe991cd279d2710e809e7354033cae78ca
Preet <preetakamonk@gmail.com>
2025-09-03 21:01:03 +0530

    Update firebase.ts

--- src/firebase.ts/src/firebase.ts
+++ src/firebase.ts/src/firebase.ts
@@ -7,7 +7,7 @@
 // // Your web app's Firebase configuration
 // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // const firebaseConfig = {
-//   apiKey: "                                          preett!!",
+//   apiKey: "                                          haha you thought",
 //   authDomain: "student-web-dev.firebaseapp.com",
 //   projectId: "student-web-dev",
 //   storageBucket: "student-web-dev.firebasestorage.app",
